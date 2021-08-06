d393f55e536a0003611c5e46d30a9ca851f87421
abdu-zeyad <abd.d@live.com>
2021-08-06 16:49:00 +0300

    sa

--- python/linked_list/linked_list/linked_list.py
+++ python/linked_list/linked_list/linked_list.py
@@ -34,6 +34,43 @@ class Linked_list:
             current = current.next
         return False
 
+    def append(self, value):
+
+        new_node = Node(value)
+        current = self.head
+
+        if current == None:
+           current = new_node
+        else:
+            while current.next != None:
+                current = current.next
+        current.next = new_node
+
+
+    def insert_before(self,value,new_value):
+        node = Node(new_value)
+        current_node=self.head
+        if current_node.value==value:
+             node.next = self.head
+             self.head = node
+        else:
+            while current_node.next:
+                if current_node.next.value==value:
+                    node.next=current_node.next
+                    current_node.next=node
+                    break
+                current_node=current_node.next
+
+    def insert_after(self, value, new_value):
+        new_node = Node(new_value)
+        current = self.head
+        while current:
+            if current.value == value:
+                new_node.next = current.next
+                current.next = new_node
+                break
+            current = current.next
+
     def __str__(self):
 
         string = ''
@@ -51,4 +88,9 @@ if __name__ == "__main__":
     linked_list.insert(3)
     linked_list.insert(2)
     linked_list.insert(1)
+    linked_list.append(11)
+    linked_list.insert_before(11,8)
+    linked_list.insert_after(11,8)
+    linked_list.insert_after(1,8)
+    linked_list.insert_before(1,8)
     print(linked_list)
--- python/linked_list/tests/test_linked_list.py
+++ python/linked_list/tests/test_linked_list.py
@@ -1,6 +1,5 @@
-
-
 from linked_list import __version__
+import linked_list
 
 from linked_list.linked_list import Linked_list
 
@@ -22,3 +21,73 @@ def test_str():  # test 4
     ll.insert(74)
     ll.__str__()
     assert ll.__str__() == "{74} -> {4} -> {'a'} -> Null"
+
+########### for code challange 06: #############
+########### linked-list-insertions #############
+
+def test_append():
+    linked_list = Linked_list()
+    linked_list.insert(7)
+    linked_list.insert(4)
+    linked_list.append(74)
+    linked_list.__str__()
+    # actual = str(ll)
+    # expected = '{10} -> {6} -> {3} -> {74} -> NULL'
+    # assert actual == expected
+    assert linked_list.__str__() == "{4} -> {7} -> {74} -> Null"
+
+def test_multiple_append():
+    linked_list = Linked_list()
+    linked_list.insert(7)
+    linked_list.insert(4)
+    linked_list.append(74)
+    linked_list.append(66)
+    linked_list.__str__()
+    assert linked_list.__str__() == "{4} -> {7} -> {74} -> {66} -> Null"
+
+
+def test_insert_before_middle():
+    linked_list = Linked_list()
+    linked_list.insert(7)
+    linked_list.insert(4)
+    linked_list.insert(1)
+    linked_list.append(74)
+    linked_list.append(66)
+    linked_list.insert_before(7,'before middle')
+    linked_list.__str__()
+    assert linked_list.__str__() == "{1} -> {4} -> {'before middle'} -> {7} -> {74} -> {66} -> Null"
+
+def test_insert_before_first():
+    linked_list = Linked_list()
+    linked_list.insert(7)
+    linked_list.insert(4)
+    linked_list.insert(1)
+    linked_list.append(74)
+    linked_list.append(66)
+    linked_list.insert_before(1,100)
+    linked_list.__str__()
+    assert linked_list.__str__() == "{100} -> {1} -> {4} -> {7} -> {74} -> {66} -> Null"
+
+
+def test_insert_after_middle():
+    linked_list = Linked_list()
+    linked_list.insert(7)
+    linked_list.insert(4)
+    linked_list.insert(1)
+    linked_list.append(74)
+    linked_list.append(66)
+    linked_list.insert_after(7,'after middle')
+    linked_list.__str__()
+    assert linked_list.__str__() == "{1} -> {4} -> {7} -> {'after middle'} -> {74} -> {66} -> Null"
+
+
+def test_insert_after_end():
+    linked_list = Linked_list()
+    linked_list.insert(7)
+    linked_list.insert(4)
+    linked_list.insert(1)
+    linked_list.append(74)
+    linked_list.append(66)
+    linked_list.insert_after(66,'after end')
+    linked_list.__str__()
+    assert linked_list.__str__() == "{1} -> {4} -> {7} -> {74} -> {66} -> {'after end'} -> Null"
