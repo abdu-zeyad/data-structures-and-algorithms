be889424e9c892074733d315f5e8495382eda536
abdu-zeyad <abd.d@live.com>
2021-08-07 00:16:26 +0300

    dsd

--- python/linked_list/linked_list/linked_list.py
+++ python/linked_list/linked_list/linked_list.py
@@ -1,86 +1,77 @@
-class Node :
-    def __init__(self,value):
-        self.value=value
-        self.next=None
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
 class Linked_list:
-    def __init__(self,head=None):
+    def __init__(self, head=None):
 
-        self.head=head
+        self.head = head
 
+    def insert(self, value):
 
-    def insert(self,value):
+        new_node = Node(value)
 
-        # creating a new node in front of the Linked List
-        new_node=Node(value)
-
-        # node that comes next will become the head
         if self.head:
-            new_node.next=self.head
-
-        #Assign new_node to self.head
+            new_node.next = self.head
         self.head = new_node
 
     def includes(self, value):
-        # define the head, which is the current variable
-        # Return T/F if value is in the linked list or not
-
-        current = self.head
-
-        while (current) :
-            if current.value == value:
+        current_value = self.head
+        while (current_value):
+            if current_value.value == value:
                 return True
-            current = current.next
+            current_value = current_value.next
         return False
 
     def append(self, value):
 
         new_node = Node(value)
-        current = self.head
+        current_value = self.head
 
-        if current == None:
-           current = new_node
+        if current_value == None:
+            current_value = new_node
         else:
-            while current.next != None:
-                current = current.next
-        current.next = new_node
+            while current_value.next != None:
+                current_value = current_value.next
+        current_value.next = new_node
 
-
-    def insert_before(self,value,new_value):
+    def insert_before(self, value, new_value):
         node = Node(new_value)
-        current_node=self.head
-        if current_node.value==value:
-             node.next = self.head
-             self.head = node
+        current_value_node = self.head
+        if current_value_node.value == value:
+            node.next = self.head
+            self.head = node
         else:
-            while current_node.next:
-                if current_node.next.value==value:
-                    node.next=current_node.next
-                    current_node.next=node
+            while current_value_node.next:
+                if current_value_node.next.value == value:
+                    node.next = current_value_node.next
+                    current_value_node.next = node
                     break
-                current_node=current_node.next
+                current_value_node = current_value_node.next
 
     def insert_after(self, value, new_value):
         new_node = Node(new_value)
-        current = self.head
-        while current:
-            if current.value == value:
-                new_node.next = current.next
-                current.next = new_node
+        current_value = self.head
+        while current_value:
+            if current_value.value == value:
+                new_node.next = current_value.next
+                current_value.next = new_node
                 break
-            current = current.next
+            current_value = current_value.next
 
     def __str__(self):
 
         string = ''
-        current = self.head
-        while (current):
-            string = string + f"{ { current.value } } -> "
-            current = current.next
+        current_value = self.head
+        while (current_value):
+            string = string + f"{ { current_value.value } } -> "
+            current_value = current_value.next
         else:
             string = string + 'Null'
         return string
+
 
 if __name__ == "__main__":
 
@@ -89,8 +80,8 @@ if __name__ == "__main__":
     linked_list.insert(2)
     linked_list.insert(1)
     linked_list.append(11)
-    linked_list.insert_before(11,8)
-    linked_list.insert_after(11,8)
-    linked_list.insert_after(1,8)
-    linked_list.insert_before(1,8)
+    linked_list.insert_before(11, 8)
+    linked_list.insert_after(11, 8)
+    linked_list.insert_after(1, 8)
+    linked_list.insert_before(1, 8)
     print(linked_list)
