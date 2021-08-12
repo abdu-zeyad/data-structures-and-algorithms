55489110ebe989bc62c61e82411dbf3cad66660e
abdu-zeyad <abd.d@live.com>
2021-08-12 16:43:17 +0300

    edit the tests

--- python/stack-and-queue/stack_and_queue/bracket/Bracket.py
+++ python/stack-and-queue/stack_and_queue/bracket/Bracket.py
@@ -1,6 +1,46 @@
 import re
 # from stack_and_queue import Stack, Queue
-from stack_and_queue.stack_and_queue import (Stack, Queue, Pseudo_queue)
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        node = Node(value)
+        if self.top:
+            node.next = self.top
+            self.top = node
+        else:
+            self.top = node
+
+    def pop(self):
+        try:
+            deleted_value = self.top.value
+            temp = self.top.next
+            self.top = temp
+            temp.next = None
+            return deleted_value
+        except:
+            return "This is empty stack"
+
+    def peek(self):
+        try:
+            return self.top.value
+        except:
+            return "This is empty stack"
+
+    def isEmpty(self):
+        if self.top == None:
+            return False
+        else:
+            return True
 
 
 def validate_brackets(in_str):
