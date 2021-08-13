a734cbd4c3bc5fd0703c920dd73c5aba2439c305
abdu-zeyad <abd.d@live.com>
2021-08-13 22:05:28 +0300

    finish code 15

--- python/tree/tree/tree.py
+++ python/tree/tree/tree.py
@@ -1,37 +1,95 @@
 class Node:
-    def __init__(self, value=None):
-        self.left_child = None
-        self.right_child = None
+    def __init__(self, value):
         self.value = value
+        self.left = None
+        self.right = None
 
 
-class Binary_search_tree:
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = Node(root)
+
+    def print_tree(self, trav_type):
+        if trav_type == "pre_order":
+            return self.pre_order(self.root, "")
+        elif trav_type == "in_order":
+            return self.post_order(self.root, "")
+        elif trav_type == "post_order":
+            return self.in_order(self.root, "")
+
+    def pre_order(self, start, trav):
+        if start:
+            trav += (str(start.value) + "-")
+            trav = self.pre_order(start.left, trav)
+            trav = self.pre_order(start.right, trav)
+        return trav
+
+    def in_order(self, start, trav):
+        if start:
+            trav = self.in_order(start.left, trav)
+            trav += (str(start.value) + "-")
+            trav = self.in_order(start.right, trav)
+        return trav
+
+    def post_order(self, start, trav):
+        if start:
+            trav = self.post_order(start.left, trav)
+            trav = self.post_order(start.right, trav)
+            trav += (str(start.value) + "-")
+        return trav
+
+
+class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, value):
-        if self.root == None:
+    def add(self, value):
+        if self.root is None:
             self.root = Node(value)
         else:
-            self._insert(value, self.root)
+            self._add(value, self.root)
 
-    def _insert(self, value, cur_node):
-        if value < cur_node.value:
-            if cur_node.left_child == None:
-                cur_node.left_child = Node(value)
-            else:
-                self._insert(value, cur_node.left_node)
-        elif value > cur_node.value:
-            if cur_node.right_child == None:
-                cur_node.right_child = Node(value)
-            else:
-                self._insert(value, cur_node.right_child)
+    def _add(self, value, cur_node):
+        if cur_node.left is not None:
+            cur_node.left = Node(value)
+        elif cur_node.right is not None:
+            cur_node.right = Node(value)
+
+    def contains(self, value):
+        if self.root:
+            is_found = self._contains(value, self.root)
+            if is_found:
+                return True
+            return False
         else:
-            print('value already in the tree')
+            return None
+
+    def _contains(self, value, cur_node):
+        if value > cur_node.value and cur_node.right:
+            return self._contains(value, cur_node.right)
+        if value < cur_node.value and cur_node.left:
+            return self._contains(value, cur_node.left)
+        if value == cur_node.value:
+            return True
 
 
-binary_tree = Binary_search_tree()
-binary_tree.insert(5)
-binary_tree.insert(8)
-print(binary_tree.root.value)
-print(binary_tree.root.right_child.value)
+if __name__ == "__main__":
+    bt = BinaryTree()
+    # bt.root.right = Node(5)
+    # bt.root.left = Node(7)
+    # bt.root.left.left = Node(2)
+    # bt.root.left.right = Node(6)
+    # bt.root.left.right.left = Node(5)
+    # bt.root.left.right.right = Node(11)
+    # bt.root.right.right = Node(9)
+    # bt.root.right.right.left = Node(4)
+
+    # print(bt.print_tree("pre_order"))
+    # print(bt.print_tree("post_order"))
+    # print(bt.print_tree("in_order"))
+    # bst = BinarySearchTree()
+    # bst.add(5)
+    # bst.add(4)
+    # bst.add(6)
+    # bst.contains(5)
+    # print(bst.contains(5))
