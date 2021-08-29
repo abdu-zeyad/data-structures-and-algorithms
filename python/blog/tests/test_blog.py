56dac0593a44ca5621806a34eb22cfa72e5f0494
Abdelmajed-zeyad <abd.d@live.com>
2021-08-29 15:22:05 +0300

    finish codechellange  26

---
+++ python/blog/tests/test_blog.py
@@ -0,0 +1,16 @@
+from blog import __version__
+from blog.insertion_sort import insertionSort
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_insortion():
+    arr = [5, 4, 3, 7]
+    new_arr = insertionSort(arr)
+
+    actual = [3, 4, 5, 7]
+    excepted = new_arr
+
+    assert actual == excepted
