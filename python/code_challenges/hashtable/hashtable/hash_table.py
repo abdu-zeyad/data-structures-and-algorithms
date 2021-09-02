f6da53381f6234a8ae62d8a4dce864ef03d13a55
Abdelmajed-zeyad <abd.d@live.com>
2021-09-02 23:29:09 +0300

    add function

--- python/code_challenges/hashtable/hashtable/hash_table.py
+++ python/code_challenges/hashtable/hashtable/hash_table.py
@@ -1,12 +1,75 @@
-
 x = {"abood": 5}
 y = {"ahmad": 4}
 
 
-class Hashtable():
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Linked_list:
+    def __init__(self, head=None):
+
+        self.head = head
+
+    def insert(self, value):
+
+        new_node = Node(value)
+
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+
+    def includes(self, value):
+        current_value = self.head
+        while (current_value):
+            if current_value.value == value:
+                return True
+            current_value = current_value.next
+        return False
+
+    def append(self, value):
+
+        new_node = Node(value)
+        current_value = self.head
+
+        if current_value == None:
+            current_value = new_node
+        else:
+            while current_value.next != None:
+                current_value = current_value.next
+        current_value.next = new_node
+
+    def insert_before(self, value, new_value):
+        node = Node(new_value)
+        current_value_node = self.head
+        if current_value_node.value == value:
+            node.next = self.head
+            self.head = node
+        else:
+            while current_value_node.next:
+                if current_value_node.next.value == value:
+                    node.next = current_value_node.next
+                    current_value_node.next = node
+                    break
+                current_value_node = current_value_node.next
+
+    def insert_after(self, value, new_value):
+        new_node = Node(new_value)
+        current_value = self.head
+        while current_value:
+            if current_value.value == value:
+                new_node.next = current_value.next
+                current_value.next = new_node
+                break
+            current_value = current_value.next
+
+
+class Hashtable(Linked_list):
     def __init__(self, size=1024, prime=564):
         self.size = size
-        self.array = []
+        self.array = [None] * size
         self.prime = prime
 
     def add(self, obj):
@@ -14,15 +77,16 @@ class Hashtable():
         value = 0
         for char in key:
             value += ord(char)
-        print(value)
         index = (value * self.prime) % (self.size)
-        print(index)
-        # if self.array[index] is True:
-        #     print('hi')
-        # else:
-        #     print(index)
+        self.array[index] = obj
+        self.insert(obj)
 
 
 obj = {"khaled": 5}
 ht = Hashtable()
 ht.add(obj)
+
+
+ll = Linked_list()
+ll.insert(obj)
+print(ll.head.value)
