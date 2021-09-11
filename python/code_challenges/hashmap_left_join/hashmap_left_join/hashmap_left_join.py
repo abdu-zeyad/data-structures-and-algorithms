9b759cd8c84346a12c1ad382746df42154a4c731
Abdelmajed-zeyad <abd.d@live.com>
2021-09-11 15:18:11 +0300

    finish code challenge 32

--- python/code_challenges/hashmap_left_join/hashmap_left_join/hashmap_left_join.py
+++ python/code_challenges/hashmap_left_join/hashmap_left_join/hashmap_left_join.py
@@ -2,21 +2,22 @@ from python.code_challenges.hashtable.hashtable.hash_table import Hashtable
 
 
 def left_join(ht1, ht2):
-    array2 = []
-    array1 = []
-    for i in ht2.array:
-        if i:
-            array2.append(i.head.value)
-
+    intersection_output = []
     for i in ht1.array:
         if i:
-            array1.append(i.head.value)
+            intersection = []
+            current_value = i.head
+            while current_value:
+                intersection.append(current_value.value[0])
+                intersection.append(current_value.value[1])
+                if ht2.contains(current_value.value[0]):
+                    intersection.append(ht2.get(current_value.value[0]))
+                else:
+                    intersection.append(None)
+                current_value = current_value.next
 
-    for i in array1:
-        for j in array2:
-            if i[0] == j[0]:
-                j.append(i[1])
-    return array2
+            intersection_output.append(intersection)
+    return intersection_output
 
 
 ht1 = Hashtable()
@@ -26,13 +27,12 @@ ht1.add("hasan", 18)
 ht1.add("dab", 20)
 
 ht2 = Hashtable()
-ht2.add("khaled", 10)
+ht2.add("abd", 10)
 ht2.add("asx", 12)
-ht2.add("hasan", 18)
+ht2.add("ahmad", 18)
 ht2.add("hasan", 20)
-
-array = left_join(ht1, ht2)
-print(array)
+actual = left_join(ht1, ht2)
+print(actual)
 
 
 # //////////////////////////////////////////
--- python/code_challenges/hashmaprepeatedword/hashmap_repeated_word/hashmap_repeated_word.py
+++ python/code_challenges/hashmaprepeatedword/hashmap_repeated_word/hashmap_repeated_word.py
@@ -1,40 +1,19 @@
-from python.code_challenges.hashtable.hashtable.hash_table import Hashtable
+import re
+
+words_pattern = "[a-zA-Z]+"
 
 
-def left_join(ht1, ht2):
-    intersection_output = []
-    for i in ht1.array:
-        if i:
-            intersection = []
-            current_value = i.head
-            while current_value:
-                intersection.append(current_value.value[0])
-                intersection.append(current_value.value[1])
-                if ht2.contains(current_value.value[0]):
-                    intersection.append(ht2.get(current_value.value[0]))
-                else:
-                    intersection.append(None)
-                current_value = current_value.next
+def repeated_word(string: str) -> str:
+    array_of_words = re.findall(words_pattern, string, flags=re.IGNORECASE)
+    dic = {}
+    for word in array_of_words:
+        if word in dic:
+            return word
 
-            intersection_output.append(intersection)
-    return intersection_output
+        else:
+            dic[word] = 0
 
 
-# import re
-# words_pattern = '[a-zA-Z]+'
-
-
-# def repeated_word(string: str) -> str:
-#     array_of_words = re.findall(words_pattern, string, flags=re.IGNORECASE)
-#     dic = {}
-#     for word in array_of_words:
-#         if word in dic:
-#             return word
-
-#         else:
-#             dic[word] = 0
-
-
-# string = 'this is a a test, and is this  also for the argument '
-# first_repeated_word = repeated_word(string)
-# print(first_repeated_word)
+string = "this is a a test, and is this  also for the argument "
+first_repeated_word = repeated_word(string)
+print(first_repeated_word)
