c5b036829867ca1857289184c8a1cebd65762d09
Abdelmajed-zeyad <abd.d@live.com>
2021-09-13 00:51:58 +0300

    a

--- python/code_challenges/graph/graph/graph.py
+++ python/code_challenges/graph/graph/graph.py
@@ -13,10 +13,11 @@ class Graph:
         print(self.adjList)
 
     def add_edge(self, start, end):
-        if start not in self.adjList:
-            self.adjList[start] = [{end}]
+        if start.value not in self.adjList:
+            self.adjList[start.value] = [{end.value}]
+            print(start.value)
         else:
-            self.adjList[start].append({end})
+            self.adjList[start.value].append({end.value})
 
     def size(self):
         return len(self.adjList)
@@ -37,10 +38,10 @@ graph.add_node(4)
 graph.add_node(3)
 graph.add_node(2)
 graph.add_node(1)
-graph.add_edge(1, 3)
-graph.add_edge(4, 2)
-graph.add_edge(5, 3)
-graph.add_edge(1, 5)
-graph.add_edge(2, 4)
+graph.add_edge(Node(1), Node(4))
+graph.add_edge(Node(4), Node(1))
+graph.add_edge(Node(4), Node(2))
+graph.add_edge(Node(2), Node(4))
+graph.add_edge(Node(3), Node(2))
 
 print(graph.get_neighbors(4))
